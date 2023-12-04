e68a27754b3b8ca02b30f5999e8eb32f02351cd4
Vadim Kolontsov <vadim@kolontsov.com>
2023-12-04 20:10:10 +0200

    adding day 4

---
+++ src/day04.rs
@@ -0,0 +1,7 @@
+pub fn part1(input: String) -> u64 {
+    0
+}
+
+pub fn part2(input: String) -> u64 {
+    0
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 pub type DayFn = fn(String) -> u64;
 
@@ -11,6 +12,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn, u64, u64) {
         1 => (day01::part1, day01::part2, 0, 0),
         2 => (day02::part1, day02::part2, 0, 0),
         3 => (day03::part1, day03::part2, 4361, 467835),
+        4 => (day04::part1, day04::part2, 13, 0),
         _ => {
             eprintln!("Unknown day: {}", day);
             return (nop, nop, 0, 0);
