936b685e61db305e413e43238f4a25c2f2d26ad7
Vadim Kolontsov <vadim@kolontsov.com>
2023-12-06 07:59:49 +0200

    add day 6

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod day02;
 pub mod day03;
 pub mod day04;
 pub mod day05;
+pub mod day06;
 
 pub type DayFn = fn(String) -> u64;
 
@@ -17,6 +18,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn, u64, u64) {
         3 => (day03::part1, day03::part2, 4361, 467835),
         4 => (day04::part1, day04::part2, 13, 30),
         5 => (day05::part1, day05::part2, 35, 0),
+        6 => (day06::part1, day06::part2, 288, 0),
         _ => {
             eprintln!("Unknown day: {}", day);
             return (nop, nop, 0, 0);
