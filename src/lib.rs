e52084c33e338ec7c065d6151981fe5b5f00f193
Vadim Kolontsov <vadim@kolontsov.com>
2023-12-17 10:33:01 +0200

    adding empty day 17

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub mod day13;
 pub mod day14;
 pub mod day15;
 pub mod day16;
+pub mod day17;
 
 pub type DayFn = fn(String) -> u64;
 
@@ -39,6 +40,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn, u64, u64) {
         14 => (day14::part1, day14::part2, 136, 64),
         15 => (day15::part1, day15::part2, 1320, 0),
         16 => (day16::part1, day16::part2, 46, 51),
+        17 => (day17::part1, day17::part2, 102, 0),
         _ => {
             eprintln!("Unknown day: {}", day);
             return (nop, nop, 0, 0);
